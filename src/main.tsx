c3a3a0210ed91a6250aebc037237bc5be5bc4a1c
woo642778-art <woo642778@gmail.com>
2026-08-15 08:29:58 -0700

    fix: load click-stable V2 motion overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./app/App";
 import { I18nProvider } from "./i18n/I18nContext";
 import "./app/app.css";
 import "./app/accessibility.css";
+import "./app/motion-fixes.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
