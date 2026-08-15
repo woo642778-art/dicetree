5dc5602ad769cc680bf821a87a8e1cc80e671f7b
woo642778-art <woo642778@gmail.com>
2026-08-15 08:25:42 -0700

    test: isolate V2 app renders

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,7 +1,9 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import { beforeEach, describe, expect, it } from "vitest";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import { I18nProvider } from "../i18n/I18nContext";
 import { App } from "./App";
+
+afterEach(() => cleanup());
 
 describe("V2 planner shell", () => {
   beforeEach(() => {
