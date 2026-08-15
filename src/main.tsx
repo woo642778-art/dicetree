9a14e8e6910229e56c4d36dac16718dbadbb8394
woo642778-art <woo642778@gmail.com>
2026-08-15 07:05:37 -0700

    feat: load accessibility overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import { I18nProvider } from "./i18n/I18nContext";
 import "./app/app.css";
+import "./app/accessibility.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
