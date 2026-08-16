61c10c11c2bbb53051f375e534ee61ff9620bd24
woo642778-art <woo642778@gmail.com>
2026-08-16 02:16:51 -0700

    feat: remove V2 fake-resource runtime imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./tree-data-v2/augmentTopology";
-import "./tree-data-v2/currentCorrections";
 import { App } from "./app/App";
 import { I18nProvider } from "./i18n/I18nContext";
-import { CostResearchPanel } from "./features/research/CostResearchPanel";
-import "./app/app.css";
+import "./app/tokens.css";
+import "./app/app-v3.css";
+import "./app/motion-v3.css";
 import "./app/accessibility.css";
-import "./app/motion-fixes.css";
-import "./app/cost-research.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <I18nProvider>
       <App />
-      <CostResearchPanel />
     </I18nProvider>
   </StrictMode>,
 );
