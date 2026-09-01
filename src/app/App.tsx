8318540693ee14d6ad588ee97fb4dcf0f08d53f6
woo642778-art <woo642778@gmail.com>
2026-09-01 04:30:50 -0700

    feat: show creator intro on first visit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,9 @@
 import { V3Shell } from "../features/v3/shell/V3Shell";
+import { CreatorIntroPopup } from "../features/v3/shell/CreatorIntroPopup";
 
 export function App() {
-  return <V3Shell />;
+  return <>
+    <V3Shell />
+    <CreatorIntroPopup />
+  </>;
 }
